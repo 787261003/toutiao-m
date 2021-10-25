801964cac07f711caaf93fac582d05e960182b3c
谁让我是小明呢 <9600842+xuxiao-ming@user.noreply.gitee.com>
2021-10-25 16:31:35 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载Vant全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 // 加载全局样式
 import '@/styles/index.less'
+
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
